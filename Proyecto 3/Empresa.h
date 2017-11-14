35b4db186e08e5af9eec525a534dc3482d6f924b
MiguelTornero <miguel.tornero.carrillo@gmail.com>
2017-11-13 22:06:24 -0600

    Empresa Hogar y Servicio

--- Proyecto 3/Empresa.h
+++ Proyecto 3/Empresa.h
@@ -3,4 +3,92 @@
 
 #include "Servicio.h"
 
+class Empresa:public Servicio{
+public:
+    Empresa();
+    Empresa(string, char, double, int, string, double);
+
+    void setCostoXMes(double);
+    void setCantPersonas(int);
+    void setDescripcion(string);
+    void setAdicional(double);
+
+    double getCostoXMes();
+    int getCantPersonas();
+    string getDescripcion();
+    double getAdicional();
+
+    void muestra();
+    double calculaCosto(int);
+private:
+    double costoXMes;
+    int cantPersonas;
+    string descripcion;
+    double adicional;
+};
+
+Empresa::Empresa():Servicio(){
+    costoXMes = 0.0;
+    cantPersonas = 0;
+    descripcion = "N/A";
+    adicional = 0.0;
+}
+
+Empresa::Empresa(string clave, char tipo, double costoXMes, int cantPersonas, string descripcion, double adicional):Servicio(clave, tipo){
+    this->costoXMes = costoXMes;
+    this->cantPersonas = cantPersonas;
+    this->descripcion = descripcion;
+    this->adicional = adicional;
+}
+
+void Empresa::setCostoXMes(double costoXMes){
+    this->costoXMes = costoXMes;
+}
+
+void Empresa::setCantPersonas(int cantPersonas){
+    this->cantPersonas = cantPersonas;
+}
+
+void Empresa::setDescripcion(string descripcion){
+    this->descripcion = descripcion;
+}
+
+void Empresa::setAdicional(double adicional){
+    this->adicional = adicional;
+}
+
+double Empresa::getCostoXMes(){
+    return costoXMes;
+}
+
+int Empresa::getCantPersonas(){
+    return cantPersonas;
+}
+
+string Empresa::getDescripcion(){
+    return descripcion;
+}
+
+double Empresa::getAdicional(){
+    return adicional;
+}
+
+void Empresa::muestra(){
+    cout << "Servicio: Empresa";
+    cout << "\nClave:" << clave;
+    cout << "\nTipo: " << tipo;
+    cout << "\nCosto por mes: " << costoXMes;
+    cout << "\nCantidad de personas: " << cantPersonas;
+    cout << "\nDescripcion: " << descripcion;
+    cout << "\nAdicional: " << adicional << endl;
+}
+
+double Empresa::calculaCosto(int diasDuracion){
+    diasDuracion %= 30;
+    if (cantPersonas > 200){
+        return (cantPersonas - 200) * adicional + costoXMes * diasDuracion;
+    }
+    return costoXMes * diasDuracion;
+}
+
 #endif // EMPRESA_H_INCLUDED
--- Proyecto 3/Hogar.h
+++ Proyecto 3/Hogar.h
@@ -3,4 +3,60 @@
 
 #include "Servicio.h"
 
+class Hogar:public Servicio{
+public:
+    Hogar();
+    Hogar(string, char, double, string);
+
+    void setCostoXDia(double);
+    void setDescripcion(string);
+
+    double getCostoXDia();
+    string getDescripcion();
+
+    void muestra();
+    double calculaCosto(int);
+private:
+    double costoXDia;
+    string descripcion;
+};
+
+Hogar::Hogar():Servicio(){
+    costoXDia = 0.0;
+    descripcion = "N/A";
+}
+
+Hogar::Hogar(string clave, char tipo, double costoXDia, string descripcion):Servicio(clave, tipo){
+    this->costoXDia = costoXDia;
+    this->descripcion = descripcion;
+}
+
+void Hogar::setCostoXDia(double costoXDia){
+    this->costoXDia = costoXDia;
+}
+
+void Hogar::setDescripcion(string descripcion){
+    this->descripcion = descripcion;
+}
+
+double Hogar::getCostoXDia(){
+    return costoXDia;
+}
+
+string Hogar::getDescripcion(){
+    return descripcion;
+}
+
+void Hogar::muestra(){
+    cout << "Servicio: Hogar";
+    cout << "\nClave:" << clave;
+    cout << "\nTipo: " << tipo;
+    cout << "\nCosto por dia: " << costoXDia;
+    cout << "\nDescripcion: " << descripcion << endl;
+}
+
+double Hogar::calculaCosto(int diasDuracion){
+    return costoXDia * diasDuracion;
+}
+
 #endif // HOGAR_H_INCLUDED
