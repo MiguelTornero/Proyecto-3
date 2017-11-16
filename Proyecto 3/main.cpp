8029cdfb946fb0343d04b023c4aef88033b42759
Miguel Tornero <miguel.tornero.carrillo@gmail.com>
2017-11-16 16:01:21 -0600

    something

--- Proyecto 3/main.cpp
+++ Proyecto 3/main.cpp
@@ -10,14 +10,57 @@
 #include <fstream>
 
 #include "Fecha.h"
-//#include "Contratacion.h"
+#include "Empresa.h"
+#include "Hogar.h"
 
 using namespace std;
+
+void cargarServicios(Servicio *servicios[], int tam){
+    ifstream inputFile;
+    int inInt, counter = 0;
+    double inDouble1, inDouble2;
+    char inChar;
+    string inString1, inString2, auxString;
+
+    inputFile.open("Servicios.txt");
+
+    for (int i = 0; i < tam && !inputFile.eof(); i++){
+        inputFile >> inString1 >> inChar >> inDouble1;
+        cout << i << endl;
+        if (inputFile >> inInt){
+            inputFile >> inString2;
+            while (!(inputFile >> inDouble2)){
+                inputFile.clear();
+                inputFile >> auxString;
+                inString2 += ' ' + auxString;
+            }
+            servicios[i] = new Empresa(inString1, inChar, inDouble1, inInt, inString2, inDouble2);
+            counter++;
+        }
+        else{
+            inputFile.clear();
+            getline(inputFile, inString2);
+            servicios[i] = new Hogar(inString1, inChar, inDouble1, inString2);
+            counter++;
+        }
+    }
+    inputFile.close();
+
+    for (int i = 0; i < 5; i++)
+    {
+        servicios[i]->muestra();
+        cout << endl;
+    }
+
+}
 
 int main()
 {
     Fecha fecha1, fecha2(3, 3, 3), fecha3;
-    
+    Servicio *servicios[20];
+
+    cargarServicios(servicios, 20);
+
 //    ifstream arch1;
 //    string x;
 //
@@ -29,11 +72,11 @@ int main()
 //    }
 //
 //    arch1.close();
-    
+
     cin >> fecha1;
     cout << fecha1 << endl;
     fecha1 = fecha1 + 365;
     cout << fecha1 << endl;
-    
+
     return 0;
 }
